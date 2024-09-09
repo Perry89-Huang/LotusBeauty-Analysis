a8af64f96d0dfc2a8003e5c5d6cdefa8c9179086
Perry Huang <perry@ms3.url.com.tw>
2024-09-09 21:42:08 +0800

    1st commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+import pcloudSdk from 'pcloud-sdk-js';
+
+// Create `client` using an oAuth token:
+const client = pcloudSdk.createClient('access_token');
+
+// then list root folder's contents:
+client.listfolder(0).then((fileMetadata) => {
+  console.log(fileMetadata);
+});
+
 function App() {
   return (
     <div className="App">
--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
+//index.js
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import FoodOrderApp from './FoodOrderApp';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <FoodOrderApp />
   </React.StrictMode>
 );
 
